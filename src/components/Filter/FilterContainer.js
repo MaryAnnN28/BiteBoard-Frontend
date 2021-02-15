1a773d7d108dc39941614a0dd1deb3cfbb8e125a
github username <67525690+kirstybrews@users.noreply.github.com>
2021-02-15 16:59:49 -0600

    search feature + chakra ui

--- src/components/Filter/FilterContainer.js
+++ src/components/Filter/FilterContainer.js
@@ -1,9 +1,16 @@
-import React from 'react'; 
+import React from 'react';
+import { Box, Input, InputGroup, InputLeftAddon } from "@chakra-ui/react"
+import { SearchIcon } from '@chakra-ui/icons'
 
 class FilterContainer extends React.Component {
    render() {
       return (
-         <h1>Filter Bar</h1>
+         <Box w="25%">
+            <InputGroup>
+               <InputLeftAddon children={<SearchIcon/>}/>
+               <Input placeholder="search" />
+            </InputGroup>
+         </Box>
       )
    }
 }
