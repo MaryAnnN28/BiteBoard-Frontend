4f6c2a4d39e1278ba5f78ebaf26783def7fb3b39
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-02-15 17:54:37 -0500

    render recipes page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import './App.css'
+import './App.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Route, 
--- src/components/Recipe/RecipeComponent.js
+++ src/components/Recipe/RecipeComponent.js
@@ -1,24 +1,78 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
+import './Recipes.css'
 
 
-class RecipeComponent extends React.Component {
+class RecipeComponent extends Component {
    render() {
       return (
-         <div>
-            <br />
-            <h3>{this.props.recipe.name}</h3>
-            <img src={this.props.recipe.image_url} width="40%"/>
-            <p>
-              <strong>Category: </strong> {this.props.recipe.category}<br />
-              <strong>Rating: </strong> {this.props.recipe.rating}<br />
-              <strong>Difficulty: </strong> {this.props.recipe.difficulty}<br />
-               <strong>Prep Time: </strong>{this.props.recipe.prep_time}<br />
-               <strong>Cook Time: </strong>{this.props.recipe.cook_time}<br />
+           
+      <div class="card-deck">
+         <div class="card">
+           <img class="card-img-top" src={this.props.recipe.image_url} class="card-img-top" alt="Card image cap" />
+            <div class="card-body">
+            <center><h5 class="card-title">{this.props.recipe.name}</h5></center>
+               <p class="card-text">
+                    <center> <strong>{this.props.recipe.category}</strong>  </center>
+                  <br />
+                  <strong>Level: </strong>{this.props.recipe.difficulty} &nbsp; &nbsp; &nbsp;
+            
+                  <strong>Rating: </strong> {this.props.recipe.rating}   
+               </p>
+                  <small class="text-muted">
+                     Prep Time &nbsp; <strong>{this.props.recipe.prep_time}</strong> 
+                     
+                     &nbsp; &nbsp; &nbsp; &nbsp; 
+                    
+                     Cook Time &nbsp; <strong>{this.props.recipe.cook_time}</strong>
+                  </small>
+           </div>
+            </div>
+            
 
-            </p>
-
-
+            <div class="card">
+           <img class="card-img-top" src={this.props.recipe.image_url} class="card-img-top" alt="Card image cap" />
+           <div class="card-body">
+            <center><h5 class="card-title">{this.props.recipe.name}</h5></center>
+               <p class="card-text">
+                    <center> <strong>{this.props.recipe.category}</strong>  </center>
+                  <br />
+                  <strong>Level: </strong>{this.props.recipe.difficulty} &nbsp; &nbsp; &nbsp;
+            
+                  <strong>Rating: </strong> {this.props.recipe.rating}   
+               </p>
+                  <small class="text-muted">
+                     Prep Time &nbsp; <strong>{this.props.recipe.prep_time}</strong> 
+                     
+                     &nbsp; &nbsp; &nbsp; &nbsp; 
+                    
+                     Cook Time &nbsp; <strong>{this.props.recipe.cook_time}</strong>
+                  </small>
+           </div>
+            </div>
+            
+            <div class="card">
+           <img class="card-img-top" src={this.props.recipe.image_url} class="card-img-top" alt="Card image cap" />
+           <div class="card-body">
+            <center><h5 class="card-title">{this.props.recipe.name}</h5></center>
+               <p class="card-text">
+                    <center> <strong>{this.props.recipe.category}</strong>  </center>
+                  <br />
+                  <strong>Level: </strong>{this.props.recipe.difficulty} &nbsp; &nbsp; &nbsp;
+            
+                  <strong>Rating: </strong> {this.props.recipe.rating}   
+               </p>
+                  <small class="text-muted">
+                     Prep Time &nbsp; <strong>{this.props.recipe.prep_time}</strong> 
+                     
+                     &nbsp; &nbsp; &nbsp; &nbsp; 
+                    
+                     Cook Time &nbsp; <strong>{this.props.recipe.cook_time}</strong>
+                  </small>
+           </div>
          </div>
+      </div>
+
+         
       )
    }
 }
