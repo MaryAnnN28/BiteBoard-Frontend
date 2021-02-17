67ec2c03cd6f0cab6dae22f3115bd28ac6c6ef57
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-02-17 14:51:04 -0500

    updated show page

--- src/components/Recipe/RecipeContainer.js
+++ src/components/Recipe/RecipeContainer.js
@@ -1,7 +1,10 @@
 import React from 'react'; 
 import AddRecipeCard from './AddRecipeCard'
 import RecipeComponent from './RecipeComponent';
+import RecipeShowPage from './RecipeShowPage'; 
+
 import { SimpleGrid, Box } from "@chakra-ui/react";
+
 
 class RecipeContainer extends React.Component {
 
@@ -10,12 +13,14 @@ class RecipeContainer extends React.Component {
      return (
         <Box w="80%">
             <SimpleGrid minChildWidth="18.5rem" >
-                  <AddRecipeCard />
+              <AddRecipeCard />
                   {this.props.recipes.length > 0 ? 
                      this.props.recipes.map((recipe) =>
                         <RecipeComponent key={recipe.id} recipe={recipe} />)
                      : <h1>No matches</h1>
-                  }
+              }
+              
+             
             </SimpleGrid>
        </Box>
          
